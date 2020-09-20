369275916e959a8af018773b3eeb92618b7a6a86
wts777 <1456875605@qq.com>
2020-09-20 21:33:15 +0800

    测试冲突2

--- src/main/java/com/zbj/git/ZbjGitExer.java
+++ src/main/java/com/zbj/git/ZbjGitExer.java
@@ -19,6 +19,8 @@ public class ZbjGitExer {
     }
  public void testGit() {
         System.out.println("测试冲突");
+        System.out.println("测试冲突");
+        System.out.println("测试冲突");
     }
 
 }
